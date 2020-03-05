9da56e284c63c7f6143f362f48ba3ba24d280a8c
ArdianFejzulli <fejzulliardian@gmail.com>
2020-03-05 02:59:35 +0100

    Working only me

---
+++ StockTrader -Personal/src/components/Home.vue
@@ -0,0 +1,101 @@
+<template>
+<div class="container">
+
+    <div class="row">
+      <!-- Blog Entries Column -->
+      <div class="col-md-8">
+
+        <h1 class="my-4">Page Heading
+          <small>Secondary Text</small>
+        </h1>
+        <hr />
+        <!-- Blog Post -->
+        <div class="card mb-4">
+          <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+          <div class="card-body">
+            <h2 class="card-title">Post Title</h2>
+            <p class="card-text">Text i shkurt</p>
+            <a href="#" class="btn btn-primary">Read More &rarr;</a>
+          </div>
+        </div>
+        <hr />  
+
+        <!-- Blog Post -->
+        <div class="card mb-4">
+          <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+          <div class="card-body">
+            <h2 class="card-title">Post Title</h2>
+            <p class="card-text">Text i shkurt</p>
+            <a href="#" class="btn btn-primary">Read More &rarr;</a>
+          </div>
+        </div>
+        <hr /> 
+
+        <!-- Blog Post -->
+        <div class="card mb-4">
+          <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+          <div class="card-body">
+            <h2 class="card-title">Post Title</h2>
+            <p class="card-text">Text i shkurt</p>
+            <a href="#" class="btn btn-primary">Read More &rarr;</a>
+          </div>
+        </div>
+
+        <!-- Pagination -->
+        <ul class="pagination justify-content-center mb-4">
+          <li class="page-item">
+            <a class="page-link" href="#">&larr; Older</a>
+          </li>
+          <li class="page-item disabled">
+            <a class="page-link" href="#">Newer &rarr;</a>
+          </li>
+        </ul>
+
+      </div>
+
+      <!-- Sidebar Widgets Column -->
+      <div class="col-md-4">
+
+        <!-- Search Widget -->
+        <div class="card my-4">
+          <h5 class="card-header">Search</h5>
+          <div class="card-body">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Search for...">
+              <span class="input-group-btn">
+                <button class="btn btn-success" type="button">Search</button>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <!-- Side Widget -->
+        <div class="card-body">
+            <h1> Trade or View your Portfolio</h1>
+            <h6> You may Save & Load your Data</h6>
+            <h6> Click on 'End Day' to begin a New Day!</h6>
+            <hr>
+            <p>Your Funds: {{ funds | currency }}</p>          </div>
+        </div>
+
+    </div>
+    <!-- /.row -->
+
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-black">Copyright &copy; Stock Trader 2020</p>
+        </div>
+    </footer>
+</div>
+<!-- /.container -->    
+</template>
+
+<script>
+export default {
+    computed: {
+        funds() {
+            return this.$store.getters.funds;
+        }
+    }
+}
+</script>
---
+++ StockTrader -Personal/src/data/stocks.js
@@ -0,0 +1,8 @@
+export default [
+    {id: 1, name: 'BMW', price: 110 },
+    {id: 2, name: 'Mercedes', price: 200 }, 
+    {id: 3, name: 'Apple', price: 250 },
+    {id: 4, name: 'Google', price: 80 },
+    {id: 5, name: 'Twitter', price: 830 },
+    {id: 6, name: 'Facebook', price: 840 }    
+];
